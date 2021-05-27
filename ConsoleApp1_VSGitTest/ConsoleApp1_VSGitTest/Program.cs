a728e89b4bcd1ef83dcc7a40342f165f6da17589
Ray Hildenbrand <raymond.hildenbrand@cvshealth.com>
2021-05-27 13:07:27 -0400

    Edited code in VS

--- ConsoleApp1_VSGitTest/ConsoleApp1_VSGitTest/Program.cs
+++ ConsoleApp1_VSGitTest/ConsoleApp1_VSGitTest/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1_VSGitTest
         static void Main(string[] args)
         {
             //code was edited in github
+            //code was added in visual studio
         }
     }
 }
